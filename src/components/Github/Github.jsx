cabd72e9f4067bbaf15c4ea694b2ba040dd1aa20
Leon <tahjib.leon399@gmail.com>
2023-10-25 02:08:23 +0600

    first commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+// import { useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([]);
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/leon0113')
+    //         .then(res => res.json())
+    //         .then(data => setData(data))
+    // }, [])
+
+    return (
+        <div className="text-center m-4 bg-green-400 text-white text-3xl p-4">Github followers: {data?.followers}
+            <img src={data?.avatar_url} alt="" />
+        </div>
+
+    )
+}
+
+export default Github
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/leon0113');
+    return response.json();
+}
